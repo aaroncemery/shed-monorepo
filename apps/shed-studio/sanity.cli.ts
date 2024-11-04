9855a1347f7031ed09f2d7d92ecd965adac50eb3
Aaron Emery <aaron.emery@sanity.io>
2024-11-04 10:40:13 -0800

    feat: updating sanity.cli with studioHost

--- apps/shed-studio/sanity.cli.ts
+++ apps/shed-studio/sanity.cli.ts
@@ -3,8 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '3p5kfswt',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'aarons-shed',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
