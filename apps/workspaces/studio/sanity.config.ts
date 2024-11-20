d15da4a76028dba2d967cfe0cbf2838fca44a2dd
Aaron Emery <aaron.emery@sanity.io>
2024-11-20 14:48:29 -0800

    feat: initial sanity creation

---
+++ apps/workspaces/studio/sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Workspaces',
+
+  projectId: 'jmb4q9hm',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
